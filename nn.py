45baa1fa5ed5b5fac46d5d48e17e86b75300c5e0
Maximiliano Palay <max@maximilianosmbp.ads.northwestern.edu>
2024-01-25 12:10:21 -0600

    formatted source compliant with flake8

--- nn.py
+++ nn.py
@@ -8,8 +8,10 @@ import numpy as np
 
 np.random.seed(44)
 
+
 class FCLayer():
     """ Represents a fully connected layer. """
+
     def __init__(self, name, input_size, output_size):
         self.weights = (np.random.randn(input_size, output_size))/10.0
         self.biases = np.zeros(output_size)
@@ -22,10 +24,13 @@ class FCLayer():
         return np.tanh(vector)
 
     def activation_derivative(self, vector):
-        return 1.0 - np.multiply(np.tanh(vector),np.tanh(vector))
+        return 1.0 - np.multiply(np.tanh(vector), np.tanh(vector))
+
 
 class NN():
-    """ Represents a Neural network, in this case consisting of fully connected layers. """
+    """ Represents a Neural network, in this case consisting of \
+        fully connected layers. """
+
     def __init__(self, layers):
         self._layers = layers
 
@@ -39,31 +44,33 @@ class NN():
                 inx = layer.forward(inx)
             ys.append(inx.copy())
         return ys
-    
+
     def metric(self, prediction, truth):
         return np.linalg.norm(prediction - truth)
 
-    def train(self, learning_rate, max_epochs, x_train, y_train, x_val, y_val, batch_size=1, momentum=0.6, shuffle=True, threshold=0.03):
+    def train(self, learning_rate, max_epochs, x_train, y_train, x_val, y_val,
+              batch_size=1, momentum=0.6, shuffle=True, threshold=0.03):
         """ Train using mini-batch gradient descent with momentum. """
-        
+
         # we're storing metrics to return them
         mse_train = []
         mse_val = []
-        
+
         print("starting training...")
-        
+
         # Iterate over epochs
         for epoch in range(max_epochs):
             # check we're above the threshold
             if len(mse_val) > 0 and mse_val[-1] <= threshold:
                 break
             # every 10 epochs, print metrics
-            if epoch % 10 == 0 and len(mse_val)>0:
-                print(f"epoch: {epoch}, train RMSE: {mse_train[-1]:.3E}, val RMSE: {mse_val[-1]:.3E}")
-            
+            if epoch % 10 == 0 and len(mse_val) > 0:
+                print(f"epoch: {epoch}, train RMSE: {mse_train[-1]:.3E}, \
+                      val RMSE: {mse_val[-1]:.3E}")
+
             # we store the metrics for every batch in this array
             epoch_train_mse = []
-            
+
             # shuffle the data for each epoch
             if shuffle:
                 indices = np.arange(len(x_train))
@@ -77,19 +84,23 @@ class NN():
                 y = y_train
 
             # initialize velocities for momentum
-            velocities_weights = [np.zeros_like(layer.weights) for layer in self._layers]
-            velocities_biases = [np.zeros_like(layer.biases) for layer in self._layers]
+            velocities_weights = [np.zeros_like(layer.weights) for layer
+                                  in self._layers]
+            velocities_biases = [np.zeros_like(layer.biases) for layer
+                                 in self._layers]
 
             # Iterate over batches
             for i in range(0, len(x), batch_size):
                 X_batch = x[i:i + batch_size]
                 Y_batch = y[i:i + batch_size]
-                
+
                 batch_train_mse = []
 
                 # cccumulate gradients over the batch
-                accum_gradients = [np.zeros_like(layer.weights) for layer in self._layers]
-                accum_biases = [np.zeros_like(layer.biases) for layer in self._layers]
+                accum_gradients = [np.zeros_like(layer.weights) for layer
+                                   in self._layers]
+                accum_biases = [np.zeros_like(layer.biases) for layer
+                                in self._layers]
 
                 # for each sample in the batch
                 for x_s, y_s in zip(X_batch, Y_batch):
@@ -106,18 +117,21 @@ class NN():
                     deltas = []
 
                     # add the error to this batch's metrics
-                    batch_train_mse.append(self.metric(outputs[-1],y_s))
-                    
+                    batch_train_mse.append(self.metric(outputs[-1], y_s))
+
                     # backward pass
                     output_error = y_s - outputs[-1]
-                    delta = self._layers[-1].activation_derivative(outputs[-1]) * output_error
+                    delta = self._layers[-1].\
+                        activation_derivative(outputs[-1]) * output_error
                     deltas.append(delta.copy())
 
                     layer_index = len(self._layers) - 2
                     while layer_index >= 0:
-                        sigma = np.dot(deltas[-1], self._layers[layer_index + 1].weights.transpose())
-                        delta = self._layers[layer_index].activation_derivative(outputs[layer_index])
-                        delta = np.multiply(delta,sigma)
+                        sigma = np.dot(deltas[-1], self._layers
+                                       [layer_index + 1].weights.transpose())
+                        delta = self._layers[layer_index].\
+                            activation_derivative(outputs[layer_index])
+                        delta = np.multiply(delta, sigma)
                         deltas.append(delta.copy())
                         layer_index -= 1
 
@@ -125,13 +139,20 @@ class NN():
 
                     # accumulate gradients
                     for index, layer in enumerate(self._layers):
-                        accum_gradients[index] += np.outer(inputs[index], deltas[index])
-                        accum_biases[index] += learning_rate * deltas[index].reshape(-1)
+                        accum_gradients[index] += np.outer(inputs[index],
+                                                           deltas[index])
+                        accum_biases[index] += learning_rate * deltas[index].\
+                            reshape(-1)
 
-                # update weights and biases after processing the batch with momentum
+                # update weights and biases after processing the batch with
+                # momentum
                 for index, layer in enumerate(self._layers):
-                    velocities_weights[index] = momentum * velocities_weights[index] + learning_rate * (accum_gradients[index] / batch_size)
-                    velocities_biases[index] = momentum * velocities_biases[index] + learning_rate * (accum_biases[index] / batch_size)
+                    velocities_weights[index] = momentum * \
+                        velocities_weights[index] + \
+                        learning_rate * (accum_gradients[index] / batch_size)
+                    velocities_biases[index] = momentum * \
+                        velocities_biases[index] + \
+                        learning_rate * (accum_biases[index] / batch_size)
                     layer.weights = layer.weights + velocities_weights[index]
                     layer.biases = layer.biases + velocities_biases[index]
 
@@ -143,8 +164,8 @@ class NN():
             # add the validation error
             y_val_pred = self.predict(x_val)
             temp = []
-            for a,b in zip(y_val,y_val_pred):
-                temp.append(self.metric(a,b))
+            for a, b in zip(y_val, y_val_pred):
+                temp.append(self.metric(a, b))
             mse_val.append(np.mean(temp))
 
         return mse_train, mse_val
--- run.py
+++ run.py
@@ -1,15 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from nn import *
+from nn import FCLayer, NN
 
 
 def test_network():
     """ Test neural network, aim is learning the sine function. """
 
     # generate some data
-    x = np.linspace(0,2*np.pi,2000) # even distribution across x
+    x = np.linspace(0, 2*np.pi, 2000)  # even distribution across x
     np.random.shuffle(x)            # shuffle x's
-    y = np.sin(x)+np.random.rand(x.shape[0])/10.0 # calculate y's
+    y = np.sin(x)+np.random.rand(x.shape[0])/10.0  # calculate y's
     # get the final form of X,Y
     X = np.array(x/2/np.pi)
     Y = y/y.max()
@@ -23,20 +23,24 @@ def test_network():
 
     X_val = X[split_index:]
     Y_val = Y[split_index:]
-    
+
     # set params
     lr = 0.1
     epochs = 500
 
     # instance layers & network
     layers = [FCLayer("input", 1, 20),
-              FCLayer("hidden_1", 20,20),
-              FCLayer("output", 20,1)]
+              FCLayer("hidden_1", 20, 20),
+              FCLayer("output", 20, 1)]
     nn = NN(layers)
     # train
-    train_metric, validation_metric = nn.train(learning_rate = lr,
-	max_epochs = epochs, x_train=X_train, y_train=Y_train,
-	x_val=X_val, y_val=Y_val, batch_size = 64, threshold = 0.01)
+    train_metric, validation_metric = nn.train(learning_rate=lr,
+                                               max_epochs=epochs,
+                                               x_train=X_train,
+                                               y_train=Y_train,
+                                               x_val=X_val, y_val=Y_val,
+                                               batch_size=64,
+                                               threshold=0.01)
 
     # get predictions
     preds_train = nn.predict(X_train)
@@ -45,11 +49,11 @@ def test_network():
     # plot predictions vs ground truth
     plt.figure()
     plt.scatter(X, Y)
-    plt.scatter(X_train, np.array(preds_train)[:,0])
-    plt.scatter(X_val, np.array(preds_val)[:,0])
+    plt.scatter(X_train, np.array(preds_train)[:, 0])
+    plt.scatter(X_val, np.array(preds_val)[:, 0])
     plt.title('Neural network test on learning the sine function')
     plt.legend(['ground truth', 'predictions on training set',
-	'predictions on validation set'])
+                'predictions on validation set'])
     plt.figure()
     plt.plot(train_metric)
     plt.plot(validation_metric)
@@ -58,6 +62,7 @@ def test_network():
     plt.show()
 
     return
+
 
 if __name__ == "__main__":
     test_network()
